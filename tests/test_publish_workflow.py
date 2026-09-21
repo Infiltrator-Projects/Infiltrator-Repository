6800d78e5a83925789641e451eebde48f9ccd838
Shannon Smith <infiltratr@yandex.com>
2026-09-21 21:53:16 +1000

    Test application release dispatch trigger

--- tests/test_publish_workflow.py
+++ tests/test_publish_workflow.py
@@ -9,6 +9,9 @@ assert "group: pages" in workflow
 assert "cancel-in-progress: false" in workflow
 assert "cancel-in-progress: true" not in workflow
 
+assert "repository_dispatch:" in workflow
+assert "types: [application-release]" in workflow
+
 publish_index = workflow.index("name: Publish Beta Repository")
 concurrency_index = workflow.index("concurrency:", publish_index)
 cancel_index = workflow.index("cancel-in-progress: false", concurrency_index)
